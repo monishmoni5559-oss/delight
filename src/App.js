70195a0c71d1659b34265bd6ad9bcc604a6d9621
monishmoni5559-oss <monishmoni5559@gmail.com>
2025-11-20 16:35:38 +0530

    completed navigation

--- src/App.js
+++ src/App.js
@@ -6,15 +6,25 @@ import Home from './pages/Home';
 import Earing from './pages/Earing';
 import Ring from "./pages/Ring";
 import Bangel from "./pages/Bangel";
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      {/* <Home /> */}
-      <Ring />
-      {/* <Bangel /> */}
-      {/* <Earing /> */}
+
+      <BrowserRouter>
+            <Navbar />
+
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/rings' element={<Ring/>}/>
+        <Route path='/Earing' element={<Earing/>}/>
+        <Route path='/chain' element={<Bangel/>}/>
+      </Routes>
       <Footer />
+
+      </BrowserRouter>
+
+
 
     </div>
   );
